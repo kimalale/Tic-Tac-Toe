ab740c2c43c89e6933b8c5e88910d6be62946a7f
Kim Malale <kimalale@hotmail.com>
2023-09-04 23:50:37 +0200

    Refactor driver code

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require '../lib/tictactoe.rb'
+require './lib/tictactoe.rb'
 
 #Object instance of Tictactoe
 tic_tac_toe = Game.new
@@ -10,15 +10,15 @@ tic_tac_toe.getboard
 until tic_tac_toe.plays == 0 || tic_tac_toe.win
 
   print "\nEnter row: "
-  row = gets.chomp.to_i - 1
+  row = gets.chomp.to_i
   print 'Enter col: '
-  col = gets.chomp.to_i - 1
+  col = gets.chomp.to_i
 
   tic_tac_toe.make_move(row, col) #commit the move
+  tic_tac_toe.getboard # display board information
 end
 
  puts "Draw" if !tic_tac_toe.win
- puts "#{tic_tac_toe.current_player} wins!" if tic_tac_toe.win
 
 
 
